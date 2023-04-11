10ae8bc3f11ba9604a27e3ea6def4463b28b3157
Beth Burch <beth.burch@gmail.com>
2023-04-10 19:55:52 -0500

    Studio Class 5

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,71 @@
 package test;
+// TODO: import BalancedBrackets class from main package
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
-    //TODO: add tests here
+    // TODO: Test balanced brackets in front of other characters — "[]"
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    // TODO: Test balanced brackets in front of other characters — "[]LaunchCode"
+    @Test
+    public void pairOfBracketsBeforeOtherCharacters() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
 
+    // TODO: Test balanced brackets in front of other characters — "Launch[Code]"
+    @Test
+    public void pairOfBracketsInTheMiddleOfCharacters() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    // TODO: Test balanced brackets in front of other characters — "[LaunchCode]"
+    @Test
+    public void pairOfBracketsCoveringEntireCharacters() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    // TODO: Test balanced brackets — ""
+    @Test
+    public void pairOfBracketsAreEmptyButValid() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    // TODO: Test unbalanced brackets — "[LaunchCode"
+    @Test
+    public void missingClosingBracketWithCharacters() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+    @Test
+    public void invalidFlippedFloppedBracketsWithCharacters() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+    @Test
+    public void missingClosingBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void missingOpeningBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+    @Test
+    public void invalidFlippedFloppedBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void multipleMismatchedBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]][]["));
+    }
+//  TODO: The string may contain non-bracket characters as well.
+    @Test
+    public void charactersInBracketsAreNonBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[#$%]"));
+    }
 }
